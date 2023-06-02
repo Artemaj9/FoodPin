b9ced81eea34e11050b9d5fb54678f2757ac842f
Artem <moiseev@physics.msu.ru>
2023-06-02 15:42:59 +0300

    New list design

--- FoodPin/RestaurantListView.swift
+++ FoodPin/RestaurantListView.swift
@@ -12,14 +12,31 @@ struct RestaurantListView: View {
                            "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino",
                            "upstate", "traif", "graham", "waffleandwolf", "fiveleaves",
                            "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier",                                      "bourkestreetbakery", "haigh", "palomino", "upstate",
+                            "traif", "graham", "waffleandwolf",
+                            "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
     
     var body: some View {
         List {
             ForEach(restaurantNames.indices, id: \.self) { index in
-                Text(restaurantNames[index])
+                HStack(alignment: .top, spacing: 20) {
+                    Image(restaurantImages[index])
+                        .resizable()
+                        .frame(width: 120, height: 118)
+                        .cornerRadius(20)
+                    VStack(alignment: .leading) {
+                        Text(restaurantNames[index])
+                            .font(.system(.title2,design: .rounded))
+                        Text("Type")
+                            .font(.system(.body,design: .rounded))
+                        Text("Location")
+                            .font(.system(.body,design: .rounded))
+                            .foregroundColor(.gray)
+                    }
+                }
             }
+            .listStyle(.plain)
         }
-        .listStyle(.plain)
     }
 }
 
