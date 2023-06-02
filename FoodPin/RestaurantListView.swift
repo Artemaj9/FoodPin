16f9cbc142bc0997797292e9cd98d2e7d5863335
Artem <moiseev@physics.msu.ru>
2023-06-02 17:52:15 +0300

    Add favourite button

--- FoodPin/RestaurantListView.swift
+++ FoodPin/RestaurantListView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct RestaurantListView: View {
+    @State var restaurantIsFavorites = Array(repeating: false, count: 21)
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional","Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster","forkee","posatelier","bourkestreetbakery", "haigh", "palomino", "upstate","traif", "graham","waffleandwolf","fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
     var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong","Sydney", "Sydney", "Sydney","New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
@@ -17,7 +18,7 @@ struct RestaurantListView: View {
     var body: some View {
         List {
             ForEach(restaurantNames.indices, id: \.self) { index in
-                FullImageRow(imageName: restaurantImages[index], name: restaurantNames[index], type: restaurantTypes[index], location: restaurantLocations[index])
+                FullImageRow(isFavorite: $restaurantIsFavorites[index], imageName: restaurantImages[index], name: restaurantNames[index], type: restaurantTypes[index], location: restaurantLocations[index])
             }
             .listRowSeparator(.hidden)
         }
@@ -37,6 +38,8 @@ struct RestaurantListView_Previews: PreviewProvider {
 
 struct FullImageRow: View {
     @State private var  showOptions = false
+    @State private var showEror = false
+    @Binding var isFavorite: Bool
     var imageName: String
     var name : String
     var type : String
@@ -48,14 +51,22 @@ struct FullImageRow: View {
                 .aspectRatio(contentMode: .fill)
                 .frame(height: 200)
                 .cornerRadius(20)
-            VStack(alignment: .leading) {
-                Text(name)
-                    .font(.system(.title2,design: .rounded))
-                Text(type)
-                    .font(.system(.body,design: .rounded))
-                Text(location)
-                    .font(.system(.body,design: .rounded))
-                    .foregroundColor(.gray)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(name)
+                        .font(.system(.title2,design: .rounded))
+                    Text(type)
+                        .font(.system(.body,design: .rounded))
+                    Text(location)
+                        .font(.system(.body,design: .rounded))
+                        .foregroundColor(.gray)
+                }
+                if isFavorite {
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                        
+                }
             }
         }
         .onTapGesture {
@@ -63,16 +74,25 @@ struct FullImageRow: View {
         }
         .confirmationDialog("What do you want to do?", isPresented: $showOptions, titleVisibility: .visible) {
                 Button("Reserve a table") {
+                    self.showEror.toggle()
                 }
                 
                 Button("Mark as favorite") {
+                    self.showEror.toggle()
                 }
             
                 Button("Mark as favorite 2") {
+                    self.isFavorite.toggle()
                 }
-                Button("You are Daldon!") {
+                Button("Are you Daldon?") {
+                    self.showEror.toggle()
                 }
                
+        }
+        .alert("Not available for Daldons!", isPresented: $showEror) {
+            Button("OK") {}
+        } message: {
+            Text("Sorry, but you are great Daldoon! Please retry later.")
         }
     }
 }
