baa895a4d47a0c60e2766b01655efd8b29f47a66
Artem <moiseev@physics.msu.ru>
2023-06-02 14:46:25 +0300

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Artem on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
